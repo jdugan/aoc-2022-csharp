2b216db51d8fc8dc47e098a93ddcd1dcb27862e9
John Dugan <34268+jdugan@users.noreply.github.com>
2022-12-01 10:16:48 +0100

    Day 1

--- src/Aoc.Day01/Runner.cs
+++ src/Aoc.Day01/Runner.cs
@@ -1,4 +1,5 @@
-using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 using Aoc.Utility;
 
 ﻿namespace Aoc.Day01;
@@ -16,21 +17,47 @@ public class Runner
 
   public int Puzzle1()
   {
-    return -1;
+    return this.Rations()[0];
   }
 
   public int Puzzle2()
   {
-    return -2;
+    return this.Rations().GetRange(0, 3).Sum();
   }
+
 
   //--------------------------------------------------------
   // Private Methods
   //--------------------------------------------------------
 
+  // ========== DATA =======================================
+
   private string[] Data()
   {
-    string[] lines = Reader.ToLines("data/day01/input.txt");
+    string[] lines = Reader.GetLines("data/day01/input.txt");
     return lines;
+  }
+
+  private List<int> Rations()
+  {
+    var rations = new List<int>();
+    var curr    = 0;
+
+    foreach (string line in this.Data())
+    {
+      if (line == "")
+      {
+        rations.Add(curr);
+        curr = 0;
+      }
+      else
+      {
+        curr += Int32.Parse(line);
+      }
+    }
+    rations.Sort();
+    rations.Reverse();
+
+    return rations;
   }
 }
