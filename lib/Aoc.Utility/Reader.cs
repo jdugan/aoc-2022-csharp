d8ca9820053e32f8f092467e59cbed2c7500fdca
John Dugan <34268+jdugan@users.noreply.github.com>
2022-12-01 10:16:48 +0100

    Day 1

--- lib/Aoc.Utility/Reader.cs
+++ lib/Aoc.Utility/Reader.cs
@@ -1,4 +1,6 @@
 using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 ﻿namespace Aoc.Utility;
 
@@ -6,13 +8,26 @@ public class Reader
 {
   public static int[] ToIntegers(string path)
   {
-    string[] lines = Reader.ToLines(path);
-    return lines.Select(s => Convert.ToInt32(s)).ToArray();
+    string[] lines = Reader.ToStrings(path);
+    return lines.Select(s => Int32.Parse(s)).ToArray();
   }
 
-  public static string[] ToLines(string path)
+  public static string[] ToStrings(string path)
   {
-    string[] lines = System.IO.File.ReadAllLines(path);
-    return lines.Select(s => s.Trim()).Where(s => s != "").ToArray();
+    string[] lines = Reader.GetLines(path);
+    return lines.Where(s => s != "").ToArray();
+  }
+
+  public static string[] GetLines(string path)
+  {
+    // convert relative path to full path
+    Regex  re    = new Regex(@"app|test");
+    string dir   = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+    int    index = re.Matches(dir)[0].Groups[0].Captures[0].Index;
+    string fpath = Path.Join(dir.Substring(0, index), path);
+
+    // read file and read lines
+    string[] lines = System.IO.File.ReadAllLines(fpath);
+    return lines.Select(s => s.Trim()).ToArray();
   }
 }
--- test/Aoc.RegressionTests/SanityChecks.cs
+++ test/Aoc.RegressionTests/SanityChecks.cs
@@ -10,7 +10,7 @@ public class SanityChecks
     {
       var runner = new Aoc.Day01.Runner();
 
-      Assert.True(runner.Puzzle1() == -1);
-      Assert.True(runner.Puzzle2() == -2);
+      Assert.True(runner.Puzzle1() == 69177);
+      Assert.True(runner.Puzzle2() == 207456);
     }
 }
