125b81fefa1924f92f037128d3ad815953f6de6e
John Dugan <34268+jdugan@users.noreply.github.com>
2022-12-09 07:22:27 +0100

    Day 9

--- src/Aoc.Day09/Runner.cs
+++ src/Aoc.Day09/Runner.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 using Aoc.Utility;
 
 ﻿namespace Aoc.Day09;
@@ -15,23 +17,68 @@ public class Runner
 
   public int Puzzle1()
   {
-    return -1;
+    return this.CountVisits(1);
   }
 
   public int Puzzle2()
   {
-    return -2;
+    return this.CountVisits(9);
   }
 
   //--------------------------------------------------------
   // Private Methods
   //--------------------------------------------------------
 
+  // ========== ROPE HELPERS ===============================
+
+  private int CountVisits(int tailSize)
+  {
+    Knot prev;
+    var  head = new Knot();
+    var  tail = new List<Knot>();
+    for (int i = 0; i < tailSize; i++)
+    {
+      tail.Add(new Knot());
+    }
+
+    foreach ((string direction, int steps) cmd in this.Commands())
+    {
+      for (int i = 0; i < cmd.steps; i++)
+      {
+        prev = head.Move(cmd.direction);
+        foreach (Knot knot in tail)
+        {
+          prev = knot.Follow(prev);
+        }
+      }
+    }
+
+    return tail[tail.Count - 1].Visits.Count;
+  }
+
+
   // ========== DATA =======================================
 
   private List<string> Data()
   {
-    List<string> lines = Reader.ToStrings("data/day09/input.txt");
-    return lines;
+    return Reader.ToStrings("data/day09/input.txt");
+  }
+
+  private List<(string, int)> Commands()
+  {
+    var cmds = new List<(string, int)>();
+    var re   = new Regex(@"^(\w) (\d+)$");
+    foreach (string line in this.Data())
+    {
+      var m = re.Match(line);
+      if (m.Success)
+      {
+        var direction = m.Groups[1].Value;
+        var steps     = Int32.Parse(m.Groups[2].Value);
+
+        cmds.Add((direction, steps));
+      }
+    }
+    return cmds;
   }
 }
