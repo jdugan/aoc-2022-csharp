71f502535437621cf9ed2c0c3b72622ca7bd0173
John Dugan <34268+jdugan@users.noreply.github.com>
2022-12-12 11:10:51 +0100

    Day 12

--- src/Aoc.Day12/Runner.cs
+++ src/Aoc.Day12/Runner.cs
@@ -1,4 +1,5 @@
-using System.Text.RegularExpressions;
+using Dijkstra.NET.Graph;
+using Dijkstra.NET.ShortestPath;
 
 using Aoc.Utility;
 
@@ -17,17 +18,138 @@ public class Runner
 
   public int Puzzle1()
   {
-    return -1;
+    var squares  = this.BuildSquares();
+    var graph    = this.BuildGraph(squares);
+    var origin   = this.FindSquare(squares, "S");
+    var terminus = this.FindSquare(squares, "E");
+    var result   = graph.Dijkstra(origin.GraphId, terminus.GraphId);
+
+    // var pathIds  = result.GetPath().ToList();
+    // foreach (var sq in squares.Values)
+    // {
+    //   if (pathIds.Contains(sq.GraphId))
+    //   {
+    //     sq.AddToPath();
+    //   }
+    // }
+    // this.PrintSquares(squares);
+
+    return result.Distance;
   }
 
   public int Puzzle2()
   {
-    return -2;
+    var squares   = this.BuildSquares();
+    var graph     = this.BuildGraph(squares);
+    var terminus  = this.FindSquare(squares, "E");
+    var originIds = new List<uint>();
+    foreach (var id in squares.Keys)
+    {
+      if (squares[id].Symbol == char.Parse("a"))
+      {
+        originIds.Add(squares[id].GraphId);
+      }
+    }
+
+    int best = Int32.MaxValue;
+    foreach (var originId in originIds)
+    {
+      var result = graph.Dijkstra(originId, terminus.GraphId);
+      if (result.Distance < best)
+      {
+        best = result.Distance;
+      }
+    }
+
+    return best;
   }
 
   //-------------------------------------------------------
   // Private Methods
   //-------------------------------------------------------
+
+  // ========== GRAPH =====================================
+
+  private Graph<Square, string> BuildGraph (Dictionary<(int, int), Square> squares)
+  {
+    // add nodes and map graph ids to squares
+    var graph = new Graph<Square, string>();
+    foreach (var sq in squares.Values)
+    {
+      uint graphId = graph.AddNode(sq);
+      sq.GraphId = graphId;
+    }
+
+    // connect squares by graph ids
+    foreach (var sq in squares.Values)
+    {
+      var nids = sq.GetNeighborIds().ToList();
+      foreach (var nid in nids)
+      {
+        try
+        {
+          var nsq  = squares[nid];
+          var diff = nsq.Elevation - sq.Elevation;
+
+          if (diff <= 1)
+          {
+            graph.Connect(sq.GraphId, nsq.GraphId, 1, "dummy");
+          }
+        }
+        catch {}  //
+      }
+    }
+
+    return graph;
+  }
+
+
+  // ========== SQUARES ===================================
+
+  private Dictionary<(int, int), Square> BuildSquares ()
+  {
+    var squares = new Dictionary<(int, int), Square>();
+    foreach (var (y, line) in this.Data().Select((v, i) => (i, v)))
+    {
+      var row = line.ToCharArray().ToList();
+      foreach (var (x, c) in row.Select((v, i) => (i, v)))
+      {
+        var tmp = new Square(x, y, c);
+        squares[tmp.Id] = tmp;
+      }
+    }
+    return squares;
+  }
+
+  public Square FindSquare (Dictionary<(int, int), Square> squares, string symbol)
+  {
+    foreach (KeyValuePair<(int, int), Square> entry in squares)
+    {
+      if (entry.Value.Symbol == symbol.ToCharArray()[0])
+      {
+        return entry.Value;
+      }
+    }
+    return null;
+  }
+
+  public void PrintSquares (Dictionary<(int, int), Square> squares)
+  {
+    var xMax = squares.Keys.ToList().Select((item, _) => item.Item1).Max() + 1;
+    var yMax = squares.Keys.ToList().Select((item, _) => item.Item2).Max() + 1;
+
+    foreach (int y in Enumerable.Range(0, yMax))
+    {
+      var row = new List<string>();
+      foreach (int x in Enumerable.Range(0, xMax))
+      {
+        var sq = squares[(x, y)];
+        row.Add(sq.Symbol.ToString());
+      }
+      Console.WriteLine(String.Join("", row));
+    }
+  }
+
 
   // ========== DATA ======================================
 
